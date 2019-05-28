c2f1f03b7c67a0931f557a99c1133560256ad443
Bruno <bruno_marin@hotmail.com>
2019-05-28 17:24:08 -0300

    Rails and DB setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+20.times do |article|
+  article = Article.create([{ title: Faker::Marketing.buzzwords }, { content: Faker::Books::Lovecraft.sentence }])
+end
